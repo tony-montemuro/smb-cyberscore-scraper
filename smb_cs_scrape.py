98ff83877255655f564f99fbdd1762c2eb50ede7
Tony Montemuro <tonyamontemuro@gmail.com>
2023-11-14 16:30:44 -0600

    updates to script to account for changes to cyberscore ui

--- smb_cs_scrape.py
+++ smb_cs_scrape.py
@@ -4,25 +4,20 @@
 # The output of this script is a series of csv files, each of which contains
 # the data for each chart of a particular category (ex: smb1 score charts)
 
-# imports: beautiful soup for web scraping 
-# requests for making HTTP requests
-# csv for creating csv files with cyberscore data
-# os for making a new directory to store csv files
-# re for regex used to remove leading 0s from strings
-from bs4 import BeautifulSoup
-import requests
-import os
-import csv
-import re
+# imports
+from bs4 import BeautifulSoup   # for parsing the heml
+import requests                 # for making http request to cyberscore
+import os                       # for making directory to store csv files
+import csv                      # for writing to csv file with cyberscore data
+import re                       # for regex used to format strings
 
+# takes a game and mode as parameters, and returns the starting and finishing chart numbers for the combination
+# if game is not one of the following:
+    # 1, 2, p, or d
+# and if mode is not one of the following:
+    # s or t
+# -1 and -1 are returned, representing an error
 def get_chart_vals(game, mode):
-    # this function takes in a game and a mode, and returns the
-    # starting and finishing chart numbers for each combination
-    # if game is not one of the following:
-        # 1, 2, p, or d
-    # and if mode is not one of the following:
-        # s or t
-    # -1 and -1 are returned, representing an error
     if game == '1':
         if mode == 's':
             return 1054, 1171
@@ -33,7 +28,6 @@ def get_chart_vals(game, mode):
             return 1401, 1550
         if mode == 't':
             return 141667, 141684
-            # return 141685, 141959
     if game == 'p':
         if mode == 's':
             return 22451, 22600
@@ -48,32 +42,31 @@ def get_chart_vals(game, mode):
     # user misentered inputs. return error codes
     return -1, -1
 
-def elongate(n):
-    # takes in the abbreviation of a name n, and returns the
-    # elongated version
-    if n == '1': return 'smb1'
-    if n == '2': return 'smb2'
-    if n == 'p': return 'smb2pal'
-    if n == 'd': return 'smbdx'
-    if n == 's': return 'score'
-    if n == 't': return 'time'
+# takes in the abbreviation `abb` of a game, and returns the elongated version
+# if abb is not one of the following: '1', '2', 'p', 'd', 's', 't'; an error code is returned
+def elongate(abb):
+    if abb == '1': return 'smb1'
+    if abb == '2': return 'smb2'
+    if abb == 'p': return 'smb2pal'
+    if abb == 'd': return 'smbdx'
+    if abb == 's': return 'score'
+    if abb == 't': return 'time'
 
     # invalid n value, return error code
     return -1
 
+# uses regex to remove leading 0 from a string representing an int
 def remove_leading_zeros_from_str(s):
-    # uses regex to remove leading 0 from a string representing an int
     regex = '^0(?!$)'
     s = re.sub(regex, '', s)
     return s
 
+# returns the index of the ith occurence of an element in a list
 def ith_index_of_element(i, l, element):
-    # returns the index of the ith occurence of an element in a python list
     return [j for j, n in enumerate(l) if n == element][i-1]
 
-def get_title(names, game, mode):
-    # takes the text scraped from the chart title, and returns the properly
-    # formatted name
+# takes the text scraped from the chart title, and returns the properly formatted level name
+def get_level_name(names, game, mode):
     start = ith_index_of_element(2, names, '→')
 
     # special case: smb2 score. must remove the '→ high score -' prefix
@@ -84,30 +77,27 @@ def get_title(names, game, mode):
     else:
         start += 1
 
-    # now, we can finally create the name variable
-    name = names[start].lower()
+    # now, we can finally create the level_name variable
+    level_name = names[start].lower()
     for i in range(start+1, len(names)):
         current = names[i].lower()
         if current != '–':
-            name += f'-{names[i].lower()}'
-    return name
+            level_name += f'-{names[i].lower()}'
+    return level_name
 
-def format_name(name):
-    # takes in an full username (first, username, and last), extracts
-    # just the 'username', and removes the apostrophes
+# takes in an full username (first, username, and last), extracts just the 'username', and removes the apostrophes
+def format_username(name):
+    username = None
     names = name.split()
     for n in names:
         if '“' in n:
-            name = n
-    name = name.replace('“', '')
-    name = name.replace('”', '')
-    return name
+            username = n
+    username = username.replace('“', '')
+    username = username.replace('”', '')
+    return username
 
+# for each chart in the range s to f, fetch the chart submissions. some charts (see variables at top) that should be ignored
 def fetch_all_submissions(s, f, game, mode):
-    # for each chart in the range s to f, fetch the chart submissions
-    # there are some charts (see skipped_charts list and skipped_range pair) 
-    # that should be ignored. if chart_num is one of these, scraping will not 
-    # be performed on that chart
     skipped_charts = [1154, 16403, 141607, 141660, 141774]
     skipped_range = (15188, 16401)
     skipped_range_2 = (141804, 141949)
@@ -128,7 +118,6 @@ def fetch_all_submissions(s, f, game, mode):
         if (chart_num >= skipped_range_2[0] and chart_num <= skipped_range_2[1]) and game == 'smb2':
             valid_chart = False
 
-
         # if valid, we can go ahead and scrape the datas
         if valid_chart:
             fetch_chart_submissions(chart_num, game, mode)
@@ -139,70 +128,72 @@ def fetch_all_submissions(s, f, game, mode):
     if game == 'smb1' and mode == 'time':
         fetch_chart_submissions(extra_chart_smb1_time, game, mode)
         
-
+# function that, given a chart number, game, and mode, fetches all submissions for that chart, and writes the result to a csv
 def fetch_chart_submissions(chartNum, game, mode):
     # request html data from chart, and create soup
     html_text = requests.get(f'https://cyberscore.me.uk/chart/{chartNum}').text
     soup = BeautifulSoup(html_text, 'lxml')
 
     # next, get the name of the chart
-    unformatted_name = soup.find('h1', class_='gamename').text.split()
-    name = get_title(unformatted_name, game, mode)
+    unformatted_name = soup.find(id='breadcrumb').text.split()
+    name = get_level_name(unformatted_name, game, mode)
     print(f'Getting {name} data...')
 
     # next, set up object used to write to a csv file
     f = open(f'chart-info/{game}/{mode}/{name}.csv', 'w', newline='')
     csv_writer = csv.writer(f)
-    csv_writer.writerow(['Position', 'Name', f'{mode.capitalize()}', 'Day', 'Month', 'Year', 'Monkey', 'Proof'])
+    csv_writer.writerow(['Name', f'{mode.capitalize()}', 'Day', 'Month', 'Year', 'Monkey', 'Proof', 'Platform'])
 
     # get the list of all submissions
-    submissions = soup.find_all('tr', class_='not-friend')
+    submissions = soup.find_all('tr', class_='chart-standard')
     for submission in submissions:
         fetch_submission(submission, csv_writer, mode)
     f.close()
 
+# function that, given a submission object s, a csv_writer, and mode parameter, grabs the data for an individual submission,
+# and writes the result to a csv file
 def fetch_submission(s, csv_writer, mode):
     # blank list which will contain a row of data
     row = []
 
-    # first, grab the position of the submission, as well as the name
-    # of who submitted
-    pos = s.find('td', class_='pos').text.replace(' ', '').strip()
+    # first, grab the name of the person who submitted the submission
     name = s.find('div', class_='user-identification').b.text
-    name = format_name(name)
+    name = format_username(name)
 
-    # next, grab position, and split into an array of strings. this details
-    # array contains the date of submission, as well as character used
+    # next, grab details, and split into an array of strings. the details contains: date of submission, monkey, and platform
     details = s.find('div', class_='details').small.text.split()
     date = details[0].split('-')
     year, month, day = date[0], remove_leading_zeros_from_str(date[1]), remove_leading_zeros_from_str(date[2])
     monkey = details[3]
+    platform = " ".join(details[5:])
 
-    # then, grab the proof, if there is one. if the score retrieved does not
-    # contain http, this means it retrieved different data, implying no 
-    # proof exists
+    # then, grab the proof, if there is one. if the proof does not contain http, this means it retrieved different data,
+    # implying no proof exists
     proof = s.find('td', class_='data').a['href']
     if 'http' not in proof:
         proof = 'none'
 
-    # then, grab the score/time
-    score = s.find('span', class_='bigger').text
+    # then, grab the record
+    record = s.find('span', class_='bigger').text.strip()
     if mode == 'time':
-        score = score[2:]
+        record = record[2:]
+    else:
+        record = record.replace(",", "")
 
     # now, append each element to row list
-    row.append(pos)
     row.append(name)
-    row.append(score)
+    row.append(record)
     row.append(day)
     row.append(month)
     row.append(year)
     row.append(monkey)
     row.append(proof)
+    row.append(platform)
 
     # finally, write data to the csv file using csv_writer
     csv_writer.writerow(row)
 
+# function that initiates the scraping process once the user has finished the initial prompt
 def begin_scrape(game, mode):
     # first, get the value of the starting end finishing chart based
     # on the game and mode
@@ -228,6 +219,7 @@ def begin_scrape(game, mode):
     fetch_all_submissions(s, f, game, mode)
     print(f'\nData collection of {game} {mode} complete!')
 
+# function that is executed when the user chooses the option to scrape all charts
 def init_scrape_all():
     # set up a loop that will select all the game types for
     # scraping, and scrape
@@ -238,6 +230,7 @@ def init_scrape_all():
             begin_scrape(game, mode)
     print('\nData collection complete!')
 
+# function that is executed when the user chooses the option to scrape only a single category
 def init_scrape():
     # prompt user to enter which game they wish to get data for
     # then, begin scrape process
@@ -247,6 +240,7 @@ def init_scrape():
     mode = input().lower()
     begin_scrape(game, mode)
 
+# main routine
 if __name__ == "__main__":
     # ask the user if they wish to scrape the entire monkey ball
     # cyberscore boards, or just one particular section
